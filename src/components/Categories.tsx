612ab5358991ae8b04415b06d36051a2cf6d82a0
zephyrus21 <pandey.piyus@gmail.com>
2021-11-12 22:04:25 +0530

    adding slog

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCategories } from "../services";
+import Link from "next/link";
 
-interface CategoriesProps {}
+const Categories: React.FC = ({}) => {
+  const [categories, setCategories] = useState([]);
 
-const Categories: React.FC<CategoriesProps> = ({}) => {
-  return <div>CAtegories</div>;
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span
+            className={`cursor-pointer block ${
+              index === categories.length - 1 ? "border-b-0" : "border-b"
+            } pb-3 mb-3`}>
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
